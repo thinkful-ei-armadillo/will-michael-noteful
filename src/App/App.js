74c1a31dbe73aa88b60ddbeeb1e6d3a452ca9e0f
William Wong <wwong1987@gmail.com>
2019-02-12 09:00:31 -0800

    completed basic boiler plate for components

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import AddFolder from './AddFolder/AddFolder';
-import AddNote from './AddNote/AddNote';
-import NoteListMain from './NoteListMain/NoteListMain';
-import NoteListNav from './NoteListNav/NoteListNav';
-import NotePageMain from './NotePageMain/NoteListMain';
-import NotePageNav from './NotePageNav/NotePageNav';
-import './dummy-store.js';
+import React, { Component } from 'react';
+// import AddFolder from './AddFolder/AddFolder';
+// import AddNote from './AddNote/AddNote';
+// import NoteListMain from './NoteListMain/NoteListMain';
+// import NoteListNav from './NoteListNav/NoteListNav';
+// import NotePageMain from './NotePageMain/NoteListMain';
+// import NotePageNav from './NotePageNav/NotePageNav';
+// import './dummy-store.js';
 
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  state = {
+    notes: [],
+    folders: [],
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Noteful React App</h1>
+      </div>
+    )
+  }
+
+
+
 }
-
 
 
 export default App;
